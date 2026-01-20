14bf2a793dea4485683a4efa3a23e0beea494a40
saeeedhany <alsaeedalbasi0ny@gmail.com>
2026-01-21 01:26:21 +0200

    initial upload to takallam blog

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+});
+
+document.querySelectorAll("[data-animate]").forEach(el => {
+  observer.observe(el);
+});
+
